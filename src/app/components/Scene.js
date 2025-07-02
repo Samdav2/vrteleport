55e1f4f31d35451f4cc7ad781cd2cb81b49d4f79
Samdav2 <dawodujohn1@gmail.com>
2025-07-02 03:04:19 +0100

    Adding some funtionality

--- src/app/components/Scene.js
+++ src/app/components/Scene.js
@@ -1,17 +1,55 @@
-
+// src/components/Scene.js
 'use client';
 
-
 import 'aframe';
+import { Entity, Scene as AFrameScene } from 'aframe-react';
+import React from 'react';
 
 
-export default function Scene({ imageUrl }) {
 
+const Scene = ({ imageUrl, mediaType }) => {
 
   return (
-    <a-scene embedded>
+    <AFrameScene embedded>
+      {mediaType === 'image' ? (
+        // Use <a-sky> for 360-degree images
+        <Entity primitive="a-sky" src={imageUrl} />
+      ) : (
 
-      <a-sky src={imageUrl}></a-sky>
-    </a-scene>
+        <Entity primitive="a-videosphere" src={imageUrl} play-on-click />
+      )}
+
+
+      <Entity primitive="a-camera">
+        <Entity primitive="a-cursor" />
+      </Entity>
+    </AFrameScene>
   );
+};
+
+
+const PlayOnClick = {
+  schema: {},
+  init: function () {
+    this.el.sceneEl.addEventListener('click', () => {
+      if (this.el.components.material && this.el.components.material.material.map) {
+        const videoEl = this.el.components.material.material.map.image;
+        if (videoEl.paused) {
+          videoEl.play();
+        } else {
+          videoEl.pause();
+        }
+      }
+    });
+  }
+};
+
+
+if (typeof window !== 'undefined' && window.AFRAME) {
+  if (!window.AFRAME.components['play-on-click']) {
+    window.AFRAME.registerComponent('play-on-click', PlayOnClick);
+  }
 }
+
+
+export default Scene;
