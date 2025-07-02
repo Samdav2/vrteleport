d257c17fec428ea97a4cc45c5b9d865c8264e946
Samdav2 <dawodujohn1@gmail.com>
2025-07-02 03:25:20 +0100

    Fixing Land Right View

--- src/app/components/Scene.js
+++ src/app/components/Scene.js
@@ -1,25 +1,18 @@
+// src/components/Scene.js
 'use client';
 
 import 'aframe';
 import React from 'react';
 
-
 export default function Scene({ mediaUrl, mediaType }) {
   return (
-
     <a-scene embedded vr-mode-ui="enabled: false">
 
       {mediaType === 'image'
-        ? <a-sky src={mediaUrl}></a-sky>
+        ? <a-sky src={mediaUrl} crossOrigin="anonymous"></a-sky>
         : <a-videosphere src={mediaUrl} autoplay="true" loop="true" crossOrigin="anonymous"></a-videosphere>
       }
-
-      <a-entity look-controls>
-
-        <a-entity camera="active: true" viewport="0 0 0.5 1" position="-0.032 0 0"></a-entity>
-        <a-entity camera="active: false" viewport="0.5 0 0.5 1" position="0.032 0 0"></a-entity>
-
-      </a-entity>
+      <a-entity camera look-controls></a-entity>
     </a-scene>
   );
 }
